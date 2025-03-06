e741c1d3c9f140b10646cade67af95bcfdee0081
poyu <chen06115@gmail.com>
2025-03-06 22:41:22 +0800

    新增標題由路由判斷

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Pageheader(){
+    //取得目前路由
+    const location = useLocation()
+
+    //對應標題
+    const pageTitles = {
+        "/":"驗證系統",
+        "/Contact":"聯絡我們"
+    }
+
+    //取得目前頁面的標題
+    const currentTitle = pageTitles[location.pathname];
+
     return(
         <>
             <header className="container-fluid mb-5">
-                <h1 className="fw-bold m-0">產品驗證系統</h1>
+                <h1 className="fw-bold m-0">{currentTitle}</h1>
                 <hr className="m-0"/>
             </header>
         </>
