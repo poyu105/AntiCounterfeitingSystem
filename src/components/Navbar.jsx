053d6b9973c6d88d34cefc983e038b1383393732
poyu <chen06115@gmail.com>
2025-03-06 22:41:04 +0800

    變更圖片路徑(上線用)，變更導覽列路由<a>-><Link>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export default function Navbar(){
     return(
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm mb-3 p-0">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">
-                        <img src="/logo-white.webp" alt="logo"/>
+                        <img src="https://raw.githubusercontent.com/poyu105/AntiCounterfeitingSystem/refs/heads/gh-pages/logo-white.webp" alt="logo"/> {/*使用開發模式請替換為:/logo-white.webp*/}
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -13,10 +14,10 @@ export default function Navbar(){
                     <div id="navbarNav" className="navbar-collapse collapse justify-content-end">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link" href="https://www.kenturn.com.tw/tw" target="blank" title="前往官網">前往官網</a>
+                                <Link className="nav-link" to="/" title="驗證系統">驗證系統</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="https://www.kenturn.com.tw/tw" target="blank" title="前往官網">前往官網</a>
+                                <Link className="nav-link" to="/Contact" title="聯絡我們">聯絡我們</Link>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="https://www.kenturn.com.tw/tw" target="blank" title="前往官網">前往官網</a>
